e3140e8986132067bccc1b5781940e799d8b3a46
rajat2130 <Kashyap.rajat2130@gmail.com>
2022-09-08 15:18:41 +0530

    Added a 404 page

--- main.go
+++ main.go
@@ -7,7 +7,17 @@ import (
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Welcome To My Awesome PixelLocked Site!</h1>")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Welcome To My Awesome PixelLocked Site!</h1>")
+
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "To get in touch, please send an email to <ahref=\"mailto:support@pixellocked.com\">support@pixellocked.com</a>.")
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "<h1>we could not find the page you were looking for :(</h1><p>Please email us if you keep being sent to an invalid page.</p>")
+
+	}
+
 }
 func main() {
 
