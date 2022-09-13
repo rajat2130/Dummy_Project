8ff269e9763c517159d24a3602f7c6ec448cbc4a
rajat2130 <Kashyap.rajat2130@gmail.com>
2022-09-13 10:05:23 +0530

    implemented the gorilla_mux router

--- main.go
+++ main.go
@@ -3,24 +3,37 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func Home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1>Welcome To My Awesome PixelLocked Site!</h1>")
+	fmt.Fprint(w, "<h1>Welcome to my Awesome Site!</h1>")
+}
 
-	} else if r.URL.Path == "/contact" {
-		fmt.Fprint(w, "To get in touch, please send an email to <ahref=\"mailto:support@pixellocked.com\">support@pixellocked.com</a>.")
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, "<h1>we could not find the page you were looking for :(</h1><p>Please email us if you keep being sent to an invalid page.</p>")
+func Contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "To get in touch, please send an email to <ahref=\"mailto:support@pixellocked.com\">support@pixellocked.com</a>.")
+}
 
-	}
+func Faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Frequently Asked Questions</h1><p>Here is a list of questions that our users commonly ask.</p>")
+}
 
+func notFound(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h1>Sorry,but we couldn't find the page you were looking for</h1>")
 }
 func main() {
+	r := mux.NewRouter()
 
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", nil)
+	r.NotFoundHandler = http.HandlerFunc(notFound)
+	r.HandleFunc("/", Home)
+	r.HandleFunc("/contact", Contact)
+	r.HandleFunc("/faq", Faq)
+
+	http.ListenAndServe(":3000", r)
 }
